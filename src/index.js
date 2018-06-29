761a4d55ecde50f2bd5106372a304ae29e9336dc
Nichlas W. Andersen <nichlas@itsnwa.com>
2018-06-29 21:53:09 +0200

    automatically set body class if fixed nav, nav toggle +++

--- src/index.js
+++ src/index.js
@@ -8,6 +8,45 @@ if (process.env.NODE_ENV === 'production') {
   document.getElementsByTagName('head')[0].appendChild(link);
 }
 
+// Navigation toggle
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target
+        var $target = document.getElementById(target)
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active')
+        $target.classList.toggle('is-active')
+
+      })
+    })
+  }
+
+})
+
+// Check for fixed nav
+const nav = document.querySelector('.navbar')
+if (nav) {
+  console.log('We got a nav!!')
+  if (nav.classList.contains('is-fixed-top')) {
+    document.body.classList.add('has-navbar-fixed-top')
+  }
+  if (nav.classList.contains('is-fixed-bottom')) {
+    document.body.classList.add('has-navbar-fixed-bottom')
+  }
+}
+
 // Dropdown toggle
 const dropdown = document.querySelector('.dropdown')
 document.addEventListener('click', function(event) {
@@ -18,4 +57,5 @@ document.addEventListener('click', function(event) {
   }
 })
 
+// Console credit
 console.info('Built with uBuild blocks theme (https://github.com/forestryio/ubuild-blocks)')
